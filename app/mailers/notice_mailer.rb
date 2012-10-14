4b09142d5b05287c52f4504b1e32a8b558dfe5e4
satoshi yamada <satoshi@kikugoro.local>
2012-10-14 14:41:41 +0900

    add forgotten files

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,19 @@
+#coding: utf-8
+
+class NoticeMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_confirm.subject
+  #
+  def sendmail_confirm(user, from, title, contents)
+    @user = user
+	@from = from
+	@title = title
+	@contents = contents
+    mail( to: "pictcollect@gmail.com",
+	      :subject => @title)
+  end
+end
