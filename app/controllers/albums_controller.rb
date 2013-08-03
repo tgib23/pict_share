b02f12213303cc14af003a9face10b385955b825
satoshi yamada <satoshi@kikugoro.local>
2013-08-03 13:44:36 +0900

    album list view fix

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,7 +4,7 @@ class AlbumsController < ApplicationController
   # GET /albums.json
   def index
 #    @albums = Album.all
-    @albums = Album.paginate(page: params[:page])
+    @albums = Album.paginate(page: params[:page], :per_page => 10)
 #    @tmp = current_album
 
     respond_to do |format|
